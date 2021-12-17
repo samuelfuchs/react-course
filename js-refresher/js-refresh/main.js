0fb90dcc2fa221f9baeb5c760e0d5df73fa309d8
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-16 21:28:18 -0300

    Arrow Functions

--- js-refresher/js-refresh/main.js
+++ js-refresher/js-refresh/main.js
@@ -1,5 +1,30 @@
-const myName = 'Max'
-console.log(myName)
+// const myName = 'Max'
+// console.log(myName)
 
-myName = 'Manu'
-console.log(myName)
+// myName = 'Manu'
+// console.log(myName)
+
+// Arrow functions
+/*
+function name(params) {
+  // ...
+}
+
+const myFnc = () => {
+  // No more issues with the this keyword!
+}
+*/
+
+// function printMyName(name) {
+//   console.log(name)
+// }
+
+// const printMyName = (name, age) => {
+//   console.log(name, age)
+// }
+
+// printMyName('Max', 28)
+
+const multiply = (number) => number * 2
+
+console.log(multiply(2))
