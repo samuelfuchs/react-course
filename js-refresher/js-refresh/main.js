5eb084f8bcb5000c5946f3ccd9c8e3f172b72130
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-16 23:46:59 -0300

    some changes

--- js-refresher/js-refresh/main.js
+++ js-refresher/js-refresh/main.js
@@ -157,10 +157,10 @@ const myFnc = () => {
 
 // ===== Refreshing Array Functions =====
 
-const numbers = [1, 2, 3]
-const doubleNumArray = numbers.map((num) => {
-  return num * 2
-})
+// const numbers = [1, 2, 3]
+// const doubleNumArray = numbers.map((num) => {
+//   return num * 2
+// })
 
-console.log(numbers)
-console.log(doubleNumArray)
+// console.log(numbers)
+// console.log(doubleNumArray)
