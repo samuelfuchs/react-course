37fabc478cc880cab730dfb5c74467fed8bb361f
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-16 21:44:12 -0300

    Understanding Classes

--- js-refresher/js-refresh/main.js
+++ js-refresher/js-refresh/main.js
@@ -32,3 +32,35 @@ const myFnc = () => {
 // ===== Exports and Imports =====
 // When exporting things we use export default xyz or we can export specific things, like constants and functions.
 // When IMPORTING things we use import xyz from './xyz.js' (we can choose a name) IF we exported as default. We use import { xyz } from './xyz.js' if we exported something specific. (we CANNOT choose a name, it must be that specific name and name it 'as' something different)
+
+// ===== Classes =====
+// classes are blueprints for JS Objects
+
+// in a class we have: properties & classes
+// properties are variables
+// methods are funcions
+class Human {
+  constructor() {
+    this.gender = 'male'
+  }
+
+  printGender() {
+    console.log(this.gender)
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super()
+    this.name = 'Max'
+    this.gender = 'female'
+  }
+
+  printMyName() {
+    console.log(this.name)
+  }
+}
+
+const person = new Person()
+person.printMyName()
+person.printGender()
